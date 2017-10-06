e03aa0c0d3c803524714ec46a62c372681891f33
Adibas03 <adibas03@ymail.com>
2017-10-06 20:28:49 +0100

    Update number formatter logic

--- utils/ethformatter.js
+++ utils/ethformatter.js
@@ -3,8 +3,10 @@ var BigNumber = require('bignumber.js');
 var Ether     = new BigNumber(10e+17);
 
 function formatAmount(amount) {
-  var ret = new BigNumber(amount.toString());
-  
-  return ret.dividedBy(Ether) + " ETH";
+  var ret = new BigNumber(amount.toString()),
+  dec = ret%Ether == 0;
+  ret = ret.dividedBy(Ether);
+  ret = !dec ? Number(ret).toFixed(20):Number(ret);
+  return ret + " ETH";
 }
 module.exports = formatAmount;
