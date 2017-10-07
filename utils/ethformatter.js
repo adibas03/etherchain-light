beb6036242d487b600ffc3b3f19426f356568003
Adibas03 <adibas03@ymail.com>
2017-10-07 05:02:33 +0100

    restore number formatter

--- utils/ethformatter.js
+++ utils/ethformatter.js
@@ -6,7 +6,11 @@ function formatAmount(amount) {
   var ret = new BigNumber(amount.toString()),
   dec = ret%Ether == 0;
   ret = ret.dividedBy(Ether);
-  ret = !dec ? Number(ret).toFixed(20):Number(ret);
+  //if(dec)
+  //ret = Number(ret);
+
+  //if(Number(ret)<1)
+  //ret = !dec ? Number(ret).toFixed(20):Number(ret);
   return ret + " ETH";
 }
 module.exports = formatAmount;
