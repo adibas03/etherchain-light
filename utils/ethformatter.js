c1451c525f234015180374f4a468b6561edba0e2
Adibas03 <adibas03@ymail.com>
2017-10-11 22:10:17 +0100

    Update ethformatter to provide, wei and gwei

--- utils/ethformatter.js
+++ utils/ethformatter.js
@@ -1,16 +1,32 @@
-var BigNumber = require('bignumber.js');
+var BigNumber = require('bignumber.js'),
+Ether     = new BigNumber(10e+17);
+Gwei = new BigNumber(10e+8);
 
-var Ether     = new BigNumber(10e+17);
 
-function formatAmount(amount) {
+function formatAmount(amount,unit) {
   var ret = new BigNumber(amount.toString()),
   dec = ret%Ether == 0;
-  ret = ret.dividedBy(Ether);
+  if(!unit){
+  ret = ret.dividedBy(Ether),
+  unit = 'ETH';
+  }
+  else{
+    switch(unit.toLowerCase()){
+      case 'wei':
+        unit = 'WEI';
+        break;
+      case 'gwei':
+        unit = 'GWEI';
+        ret = ret.dividedBy(Gwei);
+        break;
+    }
+  }
+
   //if(dec)
   //ret = Number(ret);
 
   //if(Number(ret)<1)
   //ret = !dec ? Number(ret).toFixed(20):Number(ret);
-  return ret + " ETH";
+  return ret + ' ' + unit;
 }
 module.exports = formatAmount;
